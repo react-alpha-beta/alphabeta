64b767b4cbc12c9c5a573cb2e6462397052b650b
Brian Park <yaru22@gmail.com>
2016-03-20 19:12:37 -0400

    feat: Make button components handle text prop.

--- examples/button-experiment/src/components/ButtonA/index.js
+++ examples/button-experiment/src/components/ButtonA/index.js
@@ -3,9 +3,15 @@ import React from 'react';
 import './style.css';
 
 class ButtonA extends React.Component {
+  static propTypes = {
+    text: React.PropTypes.string,
+  };
+
   render() {
     return (
-      <div className="button button-border">Button Variant A</div>
+      <div className="button button-border">
+        {this.props.text || 'Button Variant A'}
+      </div>
     );
   }
 }
--- examples/button-experiment/src/components/ButtonB/index.js
+++ examples/button-experiment/src/components/ButtonB/index.js
@@ -3,9 +3,15 @@ import React from 'react';
 import './style.css';
 
 class ButtonB extends React.Component {
+  static propTypes = {
+    text: React.PropTypes.string,
+  };
+
   render() {
     return (
-      <div className="button button-blue">Button Variant B</div>
+      <div className="button button-blue">
+        {this.props.text || 'Button Variant A'}
+      </div>
     );
   }
 }
