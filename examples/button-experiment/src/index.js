2699efd9f190b2d2936edea46be1dbbdb18e9015
Ben Hall <ben.hall@blikbook.com>
2016-03-20 18:49:42 -0400

    updating button example

--- examples/button-experiment/src/index.js
+++ examples/button-experiment/src/index.js
@@ -6,12 +6,22 @@ import './main.css';
 
 import ButtonA from './components/ButtonA';
 import ButtonB from './components/ButtonB';
+import AlphaBetaComponent from '../../../src/ABComponent';
 
 class Page extends React.Component {
+  
+  successAction(){
+  	alert("Button Clicked");
+  }
+
   render() {
     return (
       <div>
-        <ButtonA />
+        <AlphaBetaComponent
+        	successAction={this.successAction.bind(event)}
+        	ComponentA={ButtonA}
+        	ComponentB={ButtonB} 
+        	experimentParams={{"id":'123'}}/>
       </div>
     );
   }
