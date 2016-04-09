bf87110fec06e39b243128c608a36329b86d9ee5
Jack McCloy <mccloy@me.com>
2016-04-09 16:26:08 -0400

    getExperimentDataCallback returns a useable json result

--- src/Experiment.js
+++ src/Experiment.js
@@ -41,6 +41,39 @@ function assumeNormalDistrabution(trials, probabilityMean) {
   return false;
 }
 
+function testDetails(probabilityMeanDifference, marginOfError, confidenceInterval) {
+  // a function that turns the numeric data from a test into a short, human readable
+  // description of the findings
+  const differenceFloor = probabilityMeanDifference - marginOfError;
+  const differenceCeiling = probabilityMeanDifference + marginOfError;
+
+  let details;
+  const range = 'We are ' + Math.round(confidenceInterval * 100, -2) + '% confident the true difference is between ' + Math.round(differenceFloor * 100, -2) + '% and ' + Math.round(differenceCeiling * 100, -2) + '%.';
+  let recommendation;
+  if (probabilityMeanDifference > 0) {
+    details = 'Our best estimate is that the absolute rate of success is ' + Math.round(Math.abs(probabilityMeanDifference) * 100, -2) + '% higher with variant B';
+  } else {
+    details = 'Our best estimate is that the absolute rate of success is ' + Math.round(Math.abs(probabilityMeanDifference) * 100, -2) + '% lower with variant B';
+  }
+
+  if (differenceFloor * differenceCeiling > 0) {
+    details = details + ', and this result is statistically significant';
+    if (probabilityMeanDifference > 0) {
+      recommendation = 'It looks like a safe bet to go with variant B.';
+    } else {
+      recommendation = 'Given this information, you should probably stick with variant A.';
+    }
+  } else {
+    details = details + ', but this result is not statistically significant';
+    recommendation = 'You don\'t yet have enough information to make a confident decision, so you should keep running this experiment.';
+  }
+  return details + ' (' + range + '). ' + recommendation;
+}
+
+  // let description = "Our best guess is that the absolute rate of success is xxx LOWER|HIGHER for variant B as compared to variant A."
+  // description = description + "This result IS|IS NOT statistically significant to CI confidence (we are CI confident that the true difference in rate of success is between FLOOR and CEILING."
+
+
 function getExperimentDataCallback(json, confidenceInterval = 0.95) {
   const variantATrialCount =  json.variant_a_trial_count;
   const variantBTrialCount =  json.variant_b_trial_count;
@@ -64,7 +97,7 @@ function getExperimentDataCallback(json, confidenceInterval = 0.95) {
   const marginOfError = zScore * probabilityVariancePooled * Math.sqrt((1 / variantATrialCount) + (1 / variantBTrialCount));
   const differenceFloor = probabilityMeanDifference - marginOfError;
   const differenceCeiling = probabilityMeanDifference + marginOfError;
-  const confidenceIntervalAsPercentage = Math.round(confidenceInterval * 100, -2);
+  // const confidenceIntervalAsPercentage = Math.round(confidenceInterval * 100, -2);
 
   if (assumeNormalDistrabutionA === false || assumeNormalDistrabutionB === false) {
     const error = new Error('You do not have enough sample data for one or both of your variants to make an assertion');
@@ -80,14 +113,19 @@ function getExperimentDataCallback(json, confidenceInterval = 0.95) {
     throw error;
   }
 
-  console.log('differenceFloor: ' + differenceFloor);
-  console.log('differenceCeiling: ' + differenceCeiling);
-
+  let statisticalSignificance;
   if (differenceFloor * differenceCeiling > 0) {
-    console.log('We are ' + confidenceIntervalAsPercentage + '% certain that one variant is better.');
+    statisticalSignificance = true;
   } else {
-    console.log('We cannot say with ' + confidenceIntervalAsPercentage + '% certainty that one variant is better.');
+    statisticalSignificance = false;
   }
+
+  return {
+    statisticalSignificance: statisticalSignificance,
+    meanDifferenceValue: probabilityMeanDifference,
+    marginOfError: marginOfError,
+    details: testDetails(probabilityMeanDifference, marginOfError, confidenceInterval),
+  };
 }
 
 export function getExperimentData(experimentId) {
